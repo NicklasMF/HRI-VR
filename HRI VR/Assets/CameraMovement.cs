da67ff2a8d1e5e52ea8bac25ea771110c31b2729
Kalidrenlol <Thorlind89@hotmail.com>
2016-11-02 23:10:16 +0100

    * Added Robot * Added Movement to Camera * Added Movement to Door

---
+++ HRI VR/Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	private Vector3 startingPosition;
+	private Vector3 startingForward;
+	private Quaternion startingRotation;
+	public float speed = 0;
+	public float distance = 0;
+
+	void Start ()
+	{
+		startingForward = transform.forward;
+		startingPosition = transform.position;
+		startingRotation = this.transform.rotation;
+	}
+
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, startingPosition + startingForward * distance, speed * Time.deltaTime);
+	}
+}
---
+++ HRI VR/Assets/DoorMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorMovement : MonoBehaviour {
+
+	public float MoveSpeed = 0;
+	public float MaxDistance = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x + MoveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+	}
+}
