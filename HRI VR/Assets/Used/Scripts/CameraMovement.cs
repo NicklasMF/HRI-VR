38f534701058a565fe1913b2017c182650921def
Kalidrenlol <Thorlind89@hotmail.com>
2016-11-04 18:02:52 +0100

    Rebuilt and cleanup

---
+++ HRI VR/Assets/Used/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	private Vector3 startingPosition;
+	private Vector3 startingForward;
+	public float speed = 0;
+	public float distance = 0;
+
+	void Start ()
+	{
+		startingForward = transform.forward;
+		startingPosition = transform.position;
+	}
+
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, startingPosition + startingForward * distance, speed * Time.deltaTime);
+	}
+}
---
+++ HRI VR/Assets/Used/Scripts/DoorMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorMovement : MonoBehaviour {
+
+	public float MoveSpeed = 0;
+	public float MaxDistance = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < MaxDistance) {
+			transform.position = new Vector3 (transform.position.x + MoveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+	}
+}
